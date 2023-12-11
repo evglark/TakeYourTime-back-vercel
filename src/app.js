bf572e90a1af4a4f389f88c5b6483d5c144aeec8
Jewgenij Larkin <jewglark@Jewgenijs-MacBook-Pro.local>
2023-12-11 22:01:02 +0100

    Added config

--- src/app.js
+++ src/app.js
@@ -1,19 +1,30 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const session = require('express-session');
 
 require('dotenv').config();
 
+const env = require('./helpers/environments');
 const middlewares = require('./middlewares');
 const router = require('./router');
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(helmet());
-app.use(cors());
+app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(cors());
+
+app.use(session({
+	resave: false, // don't save session if unmodified
+	saveUninitialized: false, // don't create session until something stored
+	secret: env.getEnvironment('SECRET_WORD'),
+}));
 
 app.get('/', (req, res) => {
 	res.json({ message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄' });
