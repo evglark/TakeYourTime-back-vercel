40e34a6ba037f008e42932b6eb1f937722ba465a
Jewgenij Larkin <jewglark@Jewgenijs-MacBook-Pro.local>
2023-12-20 15:46:15 +0100

    Added careers route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+const careersRouter = require('../modules/career');
 const localesRouter = require('../modules/locales');
 const tasksRouter = require('../modules/tasks');
 
 const router = express.Router();
 
 router
+	.use(careersRouter)
 	.use(localesRouter)
 	.use(tasksRouter);
 
